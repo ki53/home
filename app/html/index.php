79e90f9161fe7ad3f7955f2dc1afbaa3e78380ae
ki53 <ki53jp@gmail.com>
2020-12-06 02:30:11 +0900

    some changes
    * htmlを止めてphpを利用
    * ファイルを分割

---
+++ app/html/index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="ja">
+  <?php // <head> part ?>
+  <?php include './head.php';?>
+  <body>
+    <!-- nav bar -->
+    <?php include './navbar.php';?>
+
+    <div class="raw">
+      <div class="col-4 col-md-2">left</div>
+      <div class="col-4 col-md-2">center</div>
+      <div class="col-s col-md-2">right</div>
+    </div>
+
+    <?php include '../js/js.php';?>
+  </body>
+</html>
---
+++ app/html/navbar.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+  <a href="/" class="navbar-brand">ki's home #53</a>
+  <button class="navbar-toggler" type="button"
+    data-toggle="collapse"
+    data-target="#navmenu1"
+    aria-controls="#navmenu1"
+    aria-expanded="false"
+    aria-label="Toggle navigation">
+    <span class="navbar-toggle-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navmenu1">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link" href="/otherapp/menu1.php">Menu#1</a>
+    </div>
+  </div>
+</nav>
