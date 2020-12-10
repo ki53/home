a92ac47ca7009f020d99c96abd3fe9ab089622ac
ki53 <ki53jp@gmail.com>
2020-12-10 23:56:58 +0900

    軽微な修正

--- app/html/head.php
+++ app/html/head.php
@@ -4,5 +4,5 @@
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
   <?php // other stylesheet below ?>
 
-  <title>k#53</title>
+  <title>#53</title>
 </head>
