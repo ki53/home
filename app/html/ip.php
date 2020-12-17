baf8757b63dc262d577f5ac4efb29c612090a76e
ki53 <ki53jp@gmail.com>
2020-12-17 23:20:55 +0900

    ip address table

--- app/html/ip.php
+++ app/html/ip.php
@@ -6,7 +6,20 @@
     <!-- nav bar -->
     <?php include 'common/navbar.php';?>
 
-    <?php echo $_SERVER["REMOTE_ADDR"] ; ?>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">your ip address</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <?php echo $_SERVER["REMOTE_ADDR"] ; ?>
+          </td>
+        </tr>
+      </tbody> 
+    </table>
 
     <?php include 'js/common_js.php';?>
   </body>
